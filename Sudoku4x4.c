f9632190108b2961daa43a0e64d90fc5746a4326
MCCiupek <51337012+MCCiupek@users.noreply.github.com>
2019-06-15 19:05:47 +0200

    Add files via upload

--- Sudoku4x4.c
+++ Sudoku4x4.c
@@ -6,7 +6,7 @@
 /*   By: mciupek <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/15 12:20:41 by mciupek           #+#    #+#             */
-/*   Updated: 2019/06/15 16:45:15 by mciupek          ###   ########.fr       */
+/*   Updated: 2019/06/15 19:05:37 by mciupek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,38 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-bool ft_isEmpty(int grid[4][4])
-{
-	int row;
-	int col;
+#define N 4
 
-	row = 0;
-	while (row < 4)
+int ft_isEmpty(int grid[N][N], int *row, int *col)
+{
+	int r;
+	int c;
+
+	r = *row;
+	while (r < N)
 	{
-		col = 0;
-		while (col < 4)
+		c = *col;
+		while (c < N)
 		{
-			if (grid[row][col] == 0)
+			if (grid[r][c] == 0)
+			{
+				*row = r;
+				*col = c;
 				return true;
-			col++;
+			}
+			c++;
 		}
-		row++;
+		r++;
 	}
 	return false;
 }
 
-bool ft_NotInRow(int grid[4][4], int row, int i)
+bool ft_NotInRow(int grid[N][N], int row, int i)
 {
 	int col;
 
 	col = 0;
-	while (col < 4)
+	while (col < N)
 	{
 		if (grid[row][col] == i)
 			return false;
@@ -48,12 +54,12 @@ bool ft_NotInRow(int grid[4][4], int row, int i)
 	return true;
 }
 
-bool ft_NotInCol(int grid[4][4], int col, int i)
+bool ft_NotInCol(int grid[N][N], int col, int i)
 {
 	int row;
 
 	row = 0;
-	while (row < 4)
+	while (row < N)
 	{
 		if (grid[row][col] == i)
 			return false;
@@ -63,7 +69,7 @@ bool ft_NotInCol(int grid[4][4], int col, int i)
 }
 
 
-bool ft_checkViewsH(int grid[4][4], int row, int i, int colU, int colD)
+bool ft_checkViewsH(int grid[N][N], int row, int i, int colU, int colD)
 {
 	int col;
 	int viewU;
@@ -73,7 +79,7 @@ bool ft_checkViewsH(int grid[4][4], int row, int i, int colU, int colD)
 	viewU = 1;
 	col = 1;
 	max = grid[row][0];
-	while (col < 4)
+	while (col < N)
 	{
 		if (grid[row][col] > max)
 		{
@@ -85,7 +91,7 @@ bool ft_checkViewsH(int grid[4][4], int row, int i, int colU, int colD)
 
 	viewD = 1;
 	col = 2;
-	max = grid[row][3];
+	max = grid[row][N - 1];
 	while (col >= 0)
 	{
 		if (grid[row][col] > max)
@@ -101,7 +107,7 @@ bool ft_checkViewsH(int grid[4][4], int row, int i, int colU, int colD)
 	return false;
 }
 
-bool ft_checkViewsV(int grid[4][4], int col, int i, int rowL, int rowR)
+bool ft_checkViewsV(int grid[N][N], int col, int i, int rowL, int rowR)
 {
 	int row;
 	int viewL;
@@ -111,7 +117,7 @@ bool ft_checkViewsV(int grid[4][4], int col, int i, int rowL, int rowR)
 	viewL = 1;
 	row = 1;
 	max = grid[0][col];
-	while (row < 4)
+	while (row < N)
 	{
 		if (grid[row][col] > max)
 		{
@@ -123,7 +129,7 @@ bool ft_checkViewsV(int grid[4][4], int col, int i, int rowL, int rowR)
 
 	viewR = 1;
 	row = 2;
-	max = grid[3][col];
+	max = grid[N - 1][col];
 	while (row >= 0)
 	{
 		if (grid[row][col] > max)
@@ -139,51 +145,62 @@ bool ft_checkViewsV(int grid[4][4], int col, int i, int rowL, int rowR)
 	return false;
 }
 
-/*bool ft_isConstRespected(int *grid, int row, int col, int i) 
+bool ft_isConstRespected(int grid[N][N], int view[N][N], int row, int col, int i) 
 {
-	if (ft_inRow(grid, row, i) == true &&
-			ft_inCol(grid, row, i) == true &&
-			ft_CheckViewsV(grid, col, i, rowL, rowR) == true &&
-			ft_CheckViewsH(grid, row, i, colU, colD) == true)
+	if (ft_NotInRow(grid, row, i) == true &&
+			ft_NotInCol(grid, row, i) == true &&
+			ft_checkViewsV(grid, col, i, view[0][col], view[1][col]) == true &&
+			ft_checkViewsH(grid, row, i, view[2][row], view[3][row]) == true)
 		return true;
 	return false;
 }
 
-bool ft_Solve(int **grid)
+bool ft_Solve(int grid[N][N], int view[N][N])
 {
 	int row;
 	int col;
 	int i;
-
+	
+	row = 0;
+	col = 0;
 	i = 1;
-	if (!ft_isEmpty(grid, row, col))
+	if (ft_isEmpty(grid, &row, &col) == -1)
 		return true;
 
-	while (i <= 4)
+	while (i <= N)
 	{
-		if (ft_isConstRespected(grid, row, col, i))
+		if (ft_isConstRespected(grid, view, row, col, i))
 		{
 			grid[row][col] = i;
-			if (ft_Solve(grid))
+			if (ft_Solve(grid, view))
 				return true;
 			grid[row][col] = 0;
 		}
 		i++;
 	}
 	return false;
-}*/
+}
 
 int main(void)
 {
-	int n = 4;
-	int grid[4][4] = {{1, 2, 3, 4},
+	int n = 3;
+	int row = 1;
+	int col = 1;
+	int grid[N][N] = {{1, 2, 3, 4},
 						{2, 0, 0, 0},
 						{4, 0, 0, 0},
 						{4, 0, 0, 0}};
-	printf("%d\n", ft_isEmpty(grid));
-	printf("%d\n", ft_NotInRow(grid, 1, 1));
-	printf("%d\n", ft_NotInCol(grid, 1, 1));
-	printf("%d\n", ft_checkViewsH(grid, 1, 1, 3, 2));
-	printf("%d\n", ft_checkViewsV(grid, 1, 1, 3, 2));
-	//ft_Solve(grid);
+
+	int view[N][N] = {{4, 3, 2, 1},
+						{1, 2, 2, 2},
+						{4, 3, 2, 1},
+						{1, 2, 2, 2}};
+
+	//printf("is empty = %d\n", ft_isEmpty(grid, 0, 0));
+	printf("not in row = %d\n", ft_NotInRow(grid, row, n));
+	printf("not in col = %d\n", ft_NotInCol(grid, col, n));
+	printf("views h = %d\n", ft_checkViewsH(grid, row, n, view[0][col], view[1][col]));
+	printf("views v = %d\n", ft_checkViewsV(grid, col, n, view[2][row], view[3][row]));
+	printf("TOTAL CONST = %d\n", ft_isConstRespected(grid, view, row, col, n));
+	ft_Solve(grid, view);
 }
